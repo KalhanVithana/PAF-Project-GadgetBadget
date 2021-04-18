aa82a76a63fc4f625a460426785ad5040018eaca
Dasmitha112 <dasmithagunasekara18671@gmail.com>
2021-04-18 21:03:23 +0530

    Update and recover payment

--- User/src/main/java/com/user/payment/model/payment.java
+++ User/src/main/java/com/user/payment/model/payment.java
@@ -1,7 +1,61 @@
 package com.user.payment.model;
 
-import com.user.User.UserRepository;
 
 public class payment {
-	UserRepository repo = new UserRepository();
+	private int id;
+	private int accountNo;
+	private float amount;
+	private String type;
+	private String date;
+	private String description;
+	private String buyerName;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getAccountNo() {
+		return accountNo;
+	}
+	public void setAccountNo(int accountNo) {
+		this.accountNo = accountNo;
+	}
+	public float getAmount() {
+		return amount;
+	}
+	public void setAmount(float amount) {
+		this.amount = amount;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getBuyerName() {
+		return buyerName;
+	}
+	public void setBuyerName(String buyerName) {
+		this.buyerName = buyerName;
+	}
+	
+	@Override
+	public String toString() {
+		return "Payment [id=" + id + ", accountNo=" + accountNo + ", amount=" + amount + ", type=" + type + ", date="
+				+ date + ", description=" + description + ", buyerName=" + buyerName + "]";
+	}
 }
+
--- User/src/main/java/com/user/payment/resources/paymentResources.java
+++ User/src/main/java/com/user/payment/resources/paymentResources.java
@@ -1,5 +1,79 @@
 package com.user.payment.resources;
 
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+import com.user.payment.model.payment;
+import com.user.payment.repository.paymentRepository;
+
+
+//Resources
+@Path("/payments")
 public class paymentResources {
 
+	
+paymentRepository repo = new paymentRepository();
+	
+			
+	@GET	
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<payment> getPayments() {
+		return repo.getPayments();
+	}
+	
+	@GET
+	@Path("payment/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public payment getPayment(@PathParam("id") int id) {
+		return repo.getPayment(id);
+	}
+	
+	@POST
+	@Path("payment")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public payment createPayment(payment p1) {
+		System.out.println(p1);
+		repo.create(p1);
+		
+		return p1;
+	}
+	
+	@PUT
+	@Path("payment")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public payment updatePayment(payment p1) {
+		System.out.println(p1);
+		if(repo.getPayment(p1.getId()).getId() == 0) {
+			System.out.println("Payment ID not found in the database!");
+		} else {
+			repo.update(p1);
+		}
+		return p1;
+		
+	}
+	
+	@DELETE
+	@Path("payment/{id}")
+	public payment deletePayment(@PathParam("id") int id) {
+		payment p = repo.getPayment(id);
+		
+		if(p.getId() != 0) {
+			repo.delete(id);
+		}
+		else {
+			System.out.println("Payment ID not found in the database!");
+		}
+		return p;
+		
+	}
 }
