93d793068102ed14404266ab7ee090cc07fc2b6b
Dasmitha112 <dasmithagunasekara18671@gmail.com>
2021-04-18 21:22:52 +0530

    Payment model completed!

--- User/src/main/java/com/user/payment/model/payment.java
+++ User/src/main/java/com/user/payment/model/payment.java
@@ -4,4 +4,60 @@ package com.user.payment.model;
 
 public class payment {
 	//update
+	
+	private int id;
+	private int accountNo;
+	private float amount;
+	private String type;
+	private String date;
+	private String description;
+	private String buyerName;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getAccountNo() {
+		return accountNo;
+	}
+	public void setAccountNo(int accountNo) {
+		this.accountNo = accountNo;
+	}
+	public float getAmount() {
+		return amount;
+	}
+	public void setAmount(float amount) {
+		this.amount = amount;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getBuyerName() {
+		return buyerName;
+	}
+	public void setBuyerName(String buyerName) {
+		this.buyerName = buyerName;
+	}
+	
+	@Override
+	public String toString() {
+		return "Payment [id=" + id + ", accountNo=" + accountNo + ", amount=" + amount + ", type=" + type + ", date="
+				+ date + ", description=" + description + ", buyerName=" + buyerName + "]";
+	}
 }
