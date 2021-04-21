153d953a91649b9d31f34da5dd9816f7bbc2f73d
Dasmitha112 <dasmithagunasekara18671@gmail.com>
2021-04-21 22:20:58 +0530

    Insert payment testcases completed!

--- User/src/main/java/com/user/payment/testcases/TC_paymentAPI.java
+++ User/src/main/java/com/user/payment/testcases/TC_paymentAPI.java
@@ -3,11 +3,12 @@ package com.user.payment.testcases;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import static io.restassured.RestAssured.*;
 import io.restassured.response.Response;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
+import org.json.simple.JSONObject;
+
 
 public class TC_paymentAPI {
 
@@ -17,11 +18,14 @@ public class TC_paymentAPI {
 		given()
 		
 		.when()
-			.get("http://localhost:8083/User/webapi/payments/payment/1")
+			.get("http://localhost:8083/User/webapi/payments")
 			
 		.then()
-			.statusCode(200);
+			.statusCode(200)
+			.log().body();
 	}
+	
+	
 	
 	@Test
 	public void test_getSpecificPayment() {
@@ -36,5 +40,35 @@ public class TC_paymentAPI {
 			.log().body()
 			.body("id", equalTo(1));
 	}
+	
+	
+	@Test
+	public void test_insertPayment() {
+		
+		JSONObject data = new JSONObject();
+		
+		data.put("accountNo", 554698735);
+		data.put("amount", 500000.0);
+		data.put("buyerName", "Perera");
+		data.put("date", "2021-03-11");
+		data.put("description", "Processing...");
+		data.put("id", 6);
+		data.put("type", "Credit");
+		
+
+		given()
+			.contentType("application/json")
+			.body(data)
+				
+		.when()
+			.post("http://localhost:8083/User/webapi/payments/payment")
+				
+		.then()
+			.statusCode(200)
+			.log().body();
+	}
+	
+	
+	
 
 }
